ea6bc13653f5f8a792101f36aa2a60bef3970cd9
SerginhoLeal <sergiojunioleal@hotmail.com>
2021-04-23 10:41:19 -0300

    typescript-token

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,15 +1,15 @@
-import { Request, Response, NextFunction } from 'express'
+import { Response, NextFunction } from 'express'
 import jwt from 'jsonwebtoken'
 const authConfig = process.env.GERANDO_TOKEN_LISTENER
 
-export default (req:Request, res:Response, next:NextFunction) => {
+export default (req: any, res:Response, next:NextFunction) => {
   const authHeader = req.headers.authorization
 
   if (!authHeader) return res.status(401).send({ not_provided: 'No token provided' })
 
   const parts = authHeader.split(' ')
 
-  if (!parts.length === 2) return res.status(401).send({ error: 'Token error' })
+  if (parts.length !== 2) return res.status(401).send({ error: 'Token error' })
 
   const [scheme, token] = parts
 
