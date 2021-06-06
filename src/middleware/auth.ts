9cc4f88228c1ca5ccef9e2cf1418f353c815fe41
Sergio Junio Leal <sergiojunioleal@hotmail.com>
2021-06-06 16:14:54 -0300

    jsonwebtoken

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -5,18 +5,18 @@ const authConfig = process.env.GERANDO_TOKEN_LISTENER
 export default (req: any, res:Response, next:NextFunction) => {
   const authHeader = req.headers.authorization
 
-  if (!authHeader) return res.status(401).send({ not_provided: 'No token provided' })
+  if (!authHeader) return res.status(511).send({ not_provided: 'No token provided' })
 
   const parts = authHeader.split(' ')
 
-  if (parts.length !== 2) return res.status(401).send({ error: 'Token error' })
+  if (parts.length !== 2) return res.status(511).send({ error: 'Token error' })
 
   const [scheme, token] = parts
 
-  if (!/^Bearer$/i.test(scheme)) return res.status(401).send({ error: 'Token malformatted' })
+  if (!/^Bearer$/i.test(scheme)) return res.status(511).send({ error: 'Token malformatted' })
 
-  jwt.verify(token, authConfig, (err, decoded) => {
-    if (err) return res.status(401).send({ inspired_session: 'Token invalided' })
+  jwt.verify(token, authConfig, (err: object, decoded: any) => {
+    if (err) return res.status(511).send({ inspired_session: 'Token invalided' })
 
     req.userId = decoded.id
 
